eb68daf6038849af60635b5c63f0d6a49d4eec9f
Dash <16400857+dashlers@users.noreply.github.com>
2026-05-22 23:54:37 -0500

    Create setup.ps1

---
+++ setup.ps1
@@ -0,0 +1,44 @@
+# Dashley's Install Script
+# Run this in a new Windows Sandbox or clean install to get a dev environment set up.
+# Invoke-RestMethod -Uri "https://github.com/dashlers/passwords/setup.ps1" | Invoke-Expression
+
+$startTime = Get-Date
+
+
+# Install Scoop and stuff
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+
+scoop install git nodejs-lts python
+
+scoop bucket add extras
+
+scoop install gcloud notepadplusplus firefox googlechrome keepassxc vcredist2022
+
+scoop uninstall vcredist2022
+
+reg import "$($(scoop prefix notepadplusplus))\install-context.reg"
+
+
+
+
+$endTime = Get-Date
+
+
+$elapsedTime = $endTime - $startTime
+Write-Host "Total Execution Time: $($elapsedTime.TotalSeconds) seconds"
+pause
+
+# Get credentials
+
+# & ($(scoop prefix keepassxc) + "\keepassxc-cli.exe")
+
+
+
+
+
+# create gemini project
+
+
+# npx @google/gemini-cli
